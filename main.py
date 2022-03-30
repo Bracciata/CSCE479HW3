20a7ba6dfb399f4410f11855357744dd7055db3f
Thomas Braccia <bracciata@gmail.com>
2022-03-29 19:17:53 -0500

    Added Sarats code

--- main.py
+++ main.py
@@ -9,46 +9,9 @@ from util import *
 from model import *
 DATA_DIR = './tensorflow-datasets/'
 
-# loading the dataset
-MAX_SEQ_LEN = 128
-MAX_TOKENS = 5000
 
-ds = tfds.load(
-    name="imdb_reviews", data_dir=DATA_DIR)
-# Create TextVectorization layer
-vectorize_layer = tf.keras.layers.experimental.preprocessing.TextVectorization(
-    max_tokens=MAX_TOKENS,
-    output_mode='int',
-    output_sequence_length=MAX_SEQ_LEN)
+ds=tfds.load(name="shapes3d", split="train", data_dir=DATA_DIR)
 
-# Use `adapt` to create a vocabulary mapping words to integers
-train_text = ds['train'].map(lambda x: x['text'])
-test_text = ds['test'].map(lambda x: x['text'])
-vectorize_layer.adapt(train_text)
-vectorize_layer.adapt(test_text)
-
-ExplainBaseData(ds, vectorize_layer)
-# Create the embedding layer
-VOCAB_SIZE = len(vectorize_layer.get_vocabulary())
-EMBEDDING_SIZE = int(np.sqrt(VOCAB_SIZE))
-print("Vocab size is {} and is embedded into {} dimensions".format(
-    VOCAB_SIZE, EMBEDDING_SIZE))
-
-embedding_layer = tf.keras.layers.Embedding(VOCAB_SIZE, EMBEDDING_SIZE)
-# Create, train, and display the results from Model One
-#modelOne = ModelOne(embedding_layer, vectorize_layer)
-# modelOne.train(ds)
-# modelOne.showResults()
-# Create, train, and display the results from Model Two
-# Reload the data to match the loading format for part two
-# This part is heavily inspired by https://medium.com/@nutanbhogendrasharma/sentiment-classification-with-bidirectional-lstm-on-imdb-dataset-1ab21e6eeee9
-# It aolso takes inspiration from many other tutorials
-max_features = 9999
-(x_train, y_train), (x_test, y_test) = tf.keras.datasets.imdb.load_data(
-    num_words=max_features
-)
-x_train = tf.keras.preprocessing.sequence.pad_sequences(x_train, maxlen=140)
-x_test = tf.keras.preprocessing.sequence.pad_sequences(x_test, maxlen=140)
-modelTwo = ModelTwo()
-modelTwo.train(x_train, y_train, (x_test, y_test))
-modelTwo.showResults()
+model = Model()
+batch_size = 32
+model.train(ds)
--- model.py
+++ model.py
@@ -7,128 +7,52 @@ import matplotlib.pyplot as plt    # to visualize data and draw plots
 from tqdm import tqdm              # to track progress of loops
 from util import *
 from keras_self_attention import SeqSelfAttention
+from tensorflow import keras
 
 
-class ModelOne:
-    def __init__(self, embedding_layer, vectorize_layer):
-        self.embedding_layer = embedding_layer
-        self.vectorize_layer = vectorize_layer
-
-        L2_COEFF = 0.1  # Controls how strongly to use regularization
-        # Defining, creating and calling the network repeatedly will trigger a WARNING about re-tracing the function
-        # So we'll check to see if the variable exists already
-        if 'l2_dense_net' not in locals():
-            l2_dense_net = L2DenseNetwork()
-
-        # We'll make a conv layer to produce the query and value tensors
-        self.query_layer = tf.keras.layers.Conv1D(
-            filters=100,
-            kernel_size=4,
-            padding='same')
-        self.value_layer = tf.keras.layers.Conv1D(
-            filters=100,
-            kernel_size=4,
-            padding='same')
-        # Then they will be input to the Attention layer
-        self.attention = tf.keras.layers.Attention()
-        self.concat = tf.keras.layers.Concatenate()
-
-        cells = [tf.keras.layers.LSTMCell(256), tf.keras.layers.LSTMCell(64)]
-        rnn = tf.keras.layers.RNN(cells)
-        output_layer = tf.keras.layers.Dense(1)
-        self.optimizer = tf.keras.optimizers.Adam()
-        self.model = tf.keras.Sequential(
-            [self.vectorize_layer, self.embedding_layer, rnn, output_layer])
-        self.loss_values = []
-        self.accuracy_values = []
-
-    def train(self, ds):
-        for epoch in range(1):
-            count = 0
-            for batch in ds['test'].batch(32):
-                with tf.GradientTape() as tape:
-                    text = batch['text']
-                    labels = batch['label']
-
-                    embeddings = self.embedding_layer(
-                        self.vectorize_layer(text))
-                    query = self.query_layer(embeddings)
-                    value = self.value_layer(embeddings)
-                    query_value_attention = self.attention([query, value])
-                    print("Shape after attention is (batch, seq, filters):",
-                          query_value_attention.shape)
-                    attended_values = self.concat(
-                        [query, query_value_attention])
-                    print("Shape after concatenating is (batch, seq, filters):",
-                          attended_values.shape)
-                    logits = self.model(batch['text'])
-                    loss = tf.keras.losses.binary_crossentropy(
-                        tf.expand_dims(batch['label'], -1), logits, from_logits=True)
-                    print()
-                    print("Loss values in Training set:")
-                    print(loss)
-
-                self.loss_values.append(loss)
-                # gradient update
-                grads = tape.gradient(loss, self.model.trainable_variables)
-                self.optimizer.apply_gradients(
-                    zip(grads, self.model.trainable_variables))
-                predictions = tf.argmax(logits, axis=1)
-                accuracy = tf.reduce_mean(
-                    tf.cast(tf.equal(predictions, labels), tf.float32))
-                self.accuracy_values.append(accuracy)
-                count += 1
-                if count > 50:
-                    break
-
-    def showResults(self):
-        print(self.model.summary())
-        # plot per-datum loss
-        # loss_values = np.concatenate(loss_values)
-        plt.hist(self.loss_values, density=True, bins=30)
-
-        plt.plot(self.loss_values)
-
-        plt.show()
-
-
-class ModelTwo:
+class Model:
     def __init__(self):
-        inputs = tf.keras.Input(shape=(None,), dtype="int32")
-        embedding_layer = tf.keras.layers.Embedding(
-            9999, 140)
-        model = tf.keras.Sequential()
-        model.add(inputs)
-        model.add(embedding_layer)
-        model.add(tf.keras.layers.Bidirectional(tf.keras.layers.GRU(
-            128, return_sequences=True)))
-        model.add(SeqSelfAttention(attention_activation='sigmoid'))
+        codings_size = 64
 
-        model.add(tf.keras.layers.Bidirectional(tf.keras.layers.GRU(
-            32)))
-        model.add(tf.keras.layers.Dense(16, kernel_regularizer=tf.keras.regularizers.l1_l2(
-            l1=0.001, l2=0.001), activation='relu'))
-        model.add(tf.keras.layers.Dense(1))
-        self.model = model
-        self.model.compile(loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
-                           metrics=['accuracy'])
-        # Early Stopping Callback from https://keras.io/api/callbacks/early_stopping/
-        self.callback = tf.keras.callbacks.EarlyStopping(
-            monitor='loss', patience=3)
+        self.generator = keras.models.Sequential([
+            keras.layers.Dense(100, activation="selu",
+            input_shape=(codings_size,codings_size,3)),
+            keras.layers.Dense(150, activation="selu"),
+            keras.layers.Dense (3, activation="sigmoid"),
+        ])
 
-    def train(self, xTrain, yTrain, validation):
-        self.history = self.model.fit(xTrain, yTrain,
-                                      batch_size=64,
-                                      epochs=5,
-                                      validation_data=validation,
-                                      verbose=1, callbacks=[self.callback])
 
-    def showResults(self):
-        print(self.model.summary())
-        plt.title('Loss over each Epoch')
-        plt.plot(self.history.history['loss'])
-        plt.savefig('lossRunOne.png')
+        self.discriminator = keras.models.Sequential([
+            keras.layers.Dense(150,activation="selu",input_shape=(codings_size,codings_size,3)),
+            keras.layers.Dense(100,activation="selu"),
+            keras.layers.Dense(1, activation="sigmoid")
+        ])
 
-        plt.title('Accuracy over each Epoch')
-        plt.plot(self.history.history['accuracy'])
-        plt.savefig('accuracyRunOne.png')
+        self.gan = keras.models.Sequential([self.generator,self.discriminator])
+
+        self.discriminator.compile(loss="binary_crossentropy",optimizer= "rmsprop")
+        self.discriminator.trainable = False
+        self.gan.compile(loss="binary_crossentropy", optimizer="rmsprop")
+    def train(self, dataset,codings_size=30, batch_size=32,n_epochs=50):
+       
+        for epoch in range(n_epochs):
+            print(epoch)
+            
+            for X_batch in dataset:
+                print(X_batch)
+                print(X_batch['image'])
+                # phase 1 - training the discriminator
+                noise = tf.random.normal(shape=[64,64,3])
+                generated_images = self.generator(noise)
+                x = tf.cast(X_batch['image'],tf.float32)
+                X_fake_and_real = tf.concat([generated_images,x], axis=0)
+                y1 = tf.constant([[0.]] * batch_size + [[1.1]] * batch_size)
+                self.discriminator.trainable = True
+                self.discriminator.train_on_batch(X_fake_and_real, y1)
+                # phase 2 - training the generator
+                noise = tf.random.normal(shape=[batch_size,codings_size])
+                y2 = tf.constant ([[1.]] * batch_size)
+                self.discriminator.trainable = False
+                self.gan.train_on_batch(noise, y2)
+
+   
